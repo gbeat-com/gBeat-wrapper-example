f5da6feb9ca03e3bb6ac11f0b039a62505e9d1d6
SnyderConsulting <andrew.snyder@snyderconsulting.tech>
2024-09-20 17:39:50 -0500

    Fix issue with pings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.fodsample"
         minSdk = 30
         targetSdk = 34
-        versionCode = 35
-        versionName = "1.16.1"
+        versionCode = 36
+        versionName = "1.17.1"
 
         signingConfig = signingConfigs.getByName("debug")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
